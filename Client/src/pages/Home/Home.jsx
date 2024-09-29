a56a7111281c08a67b9503ed669128c1486195fd
TheRealSourcer <bertolotti2008@gmail.com>
2024-09-29 19:17:45 -0400

    Update Home.jsx

--- Client/src/pages/Home/Home.jsx
+++ Client/src/pages/Home/Home.jsx
@@ -30,70 +30,95 @@ import SpaceshipImage2 from "../../assets/spaceship/spaceship2.jpg";
 import SpaceshipImage3 from "../../assets/spaceship/spaceship3.jpg";
 import SpaceshipImage4 from "../../assets/spaceship/spaceship4.jpg";
 
+import SmootherIcon from "../../assets/smoother/smootherIco.png";
+import BeastIcon from "../../assets/beast/beastIco.png";
+import TerminatorIcon from "../../assets/terminator/terminatorIco.png";
+import SpaceshipIcon from "../../assets/spaceship/spaceshipIco.png";
+
 const products = {
     Smoother: {
         id: "1",
         name: "Smoother",
         description: "Unleash Your Gaming Potential Without Breaking the Bank! <br>Introducing our latest budget-friendly gaming PC, designed for gamers who demand performance without the hefty price tag. This low-end gaming powerhouse is perfect for diving into the world of gaming, whether you're exploring vast open worlds, competing in intense multiplayer battles, or embarking on epic quests.",
         price: 649.99,
-        features: {
+        components: {
             "Processor": "AMD Ryzen 5 5500 6-Core, 12-Thread",
             "Memory": "TEAMGROUP T-Force Vulcan Z DDR4 DRAM 16GB Kit (2x8GB) 3200MHz",
-            "Storage": "MSI SPATIUM M450 M.2 2280 500GB PCI-Express 4.0",
+            "Storage": "MSI SPATIUM M450 M.2 2280 500GB PCI-Express  4.0",
             "Graphics Card": "ASRock Phantom RX 6500 XT ",
+            "Motherboard": "ASUS TUF Gaming A520M-PLUS (WiFi)",
+            "Processor Cooler": "AMD Wraith Stealth Cooler",
+            "Case": "Montech AIR 100 ARGB MICRO-ATX",
+            "Power Supply": "CORSAIR CX550 80 Plus Bronze 550 Watt Non Modular"
         },
         imageUrl1: SmootherImage1,
         imageUrl2: SmootherImage2,
         imageUrl3: SmootherImage3,
-        imageUrl4: SmootherImage4
+        imageUrl4: SmootherImage4,
+        icon: SmootherIcon
     },
     Beast: {
         id: "2",
         name: "Beast",
         description: "Elevate Your Gaming Experience with Our Next-Level Gaming PC! <br>Step up your game with our new gaming PC, crafted for gamers who seek a perfect balance of performance, quality, and affordability. Whether you're aiming for high scores, exploring new worlds, or streaming your gameplay, this gaming rig is built to deliver an exceptional experience - Take the next step in your gaming journey and secure your system now!",
         price: 999.99,
-        features: {
+        components: {
             "Processor": "AMD Ryzen 5 7600X 6-Core, 12-Thread ",
             "Memory": "TEAMGROUP T-Force Vulcan Z DDR4 32GB Kit (2x16GB) 3200MHz",
             "Storage": "KLEVV CRAS C910 1TB 5000MB/s",
             "Graphics Card": "ASUS Dual GeForce RTX™ 4060 OC ",
+            "Motherboard": "MSI PRO B650-S WiFi",
+            "Processor Cooler": "Cooler Master Hyper 212 120 Edge PWM Fan, 4 Copper Heat Pipes",
+            "Case": "Cooler Master MasterCase H500 ARGB",
+            "Power Supply": "AGV Series 650W Power Supply, 80 Plus Bronze Certified Non Modular"
         },
         imageUrl1: BeastImage1,
         imageUrl2: BeastImage2,
         imageUrl3: BeastImage3,
-        imageUrl4: BeastImage4
+        imageUrl4: BeastImage4,
+        icon: BeastIcon
     },
     Terminator: {
         id: "3",
         name: "Terminator",
         description: "Enhance Your Gaming Experience with Our Advanced Gaming PC! <br>Take your gaming to new heights with our advanced gaming PC, designed for enthusiasts who seek impressive performance and cutting-edge technology. Whether you're conquering the latest AAA titles, streaming your gameplay, or creating content, this rig delivers the power and precision needed to excel.",
-        price: 1499.99,
-        features: {
+        price: 1599.99,
+        components: {
             "Processor": "AMD Ryzen 7 7800X3D 8-Core, 16-Thread",
             "Memory": "TEAMGROUP T-Force Delta RGB DDR4 32GB (2x16GB) 3600MHz",
             "Storage": "118 WD_BLACK 2TB 5150 MB/s",
             "Graphics Card": "RX 7800XT XFX Speedster SWFT210 Radeon 16GB",
+            "Motherboard": "ASUS TUF Gaming A620M-PLUS(WiFi)",
+            "Processor Cooler": "ID-COOLING SE-225-XT 5 Heatpipes 2x120mm 35",
+            "Case": "Cooler Master MasterCase H500 ARGB",
+            "Power Supply": "Thermaltake GF1 ATX 750W Fully Modular"
         },
         imageUrl1: TerminatorImage1,
         imageUrl2: TerminatorImage2,
         imageUrl3: TerminatorImage3,
-        imageUrl4: TerminatorImage4
+        imageUrl4: TerminatorImage4,
+        icon: TerminatorIcon
     },
     Spaceship: {
         id: "4",
         name: "Spaceship",
-        description: "Command the Game with Our Ultimate Gaming PC! <br>Introducing the pinnacle of gaming technology: our Ultimate Gaming PC. Designed for the most demanding gamers and tech enthusiasts, this machine combines unrivaled power, cutting-edge features, and exceptional build quality. Whether you're playing the latest AAA titles, streaming in 4K, or creating high-end content, this rig delivers an unparalleled experience.",
-        price: 1999.99,
-        features: {
+        description: "Command the Game with Our Ultimate Gaming PC! <br>Introducing the pinnacle of gaming technology: our Ultimate Gaming PC. Designed for the most demanding gamers and tech enthusiasts, this machine combines unrivaled power, cutting-edge components, and exceptional build quality. Whether you're playing the latest AAA titles, streaming in 4K, or creating high-end content, this rig delivers an unparalleled experience.",
+        price: 2599.99,
+        components: {
             "Processor": "AMD Ryzen 7 7800X3D 8-Core, 16-Thread",
             "Memory": "G.SKILL Trident Z5 RGB Series (Intel XMP 3.0) DDR5 RAM 32GB (2x16GB) 7200MT/s",
             "Storage": "Acer Predator GM7000 4TB M.2 7400MB/s",
             "Graphics Card": "RX 7900XTX XFX Speedster MERC310 AMD Radeon 24GB",
+            "Motherboard": "ASUS ROG Strix B650-A Gaming WiFi 6E",
+            "Processor Cooler": "NZXT Kraken Liquid Cooling 240mm",
+            "Case": "CORSAIR iCUE 4000X RGB",
+            "Power Supply": "CORSAIR RM850x - 850w - Fully Modular ATX Power Supply - 80 PLUS Gold - Low-Noise Fan - Zero RPM"
         },
         imageUrl1: SpaceshipImage1,
         imageUrl2: SpaceshipImage2,
         imageUrl3: SpaceshipImage3,
-        imageUrl4: SpaceshipImage4
+        imageUrl4: SpaceshipImage4,
+        icon: SpaceshipIcon
     }
 };
 
@@ -188,9 +213,8 @@ export default function Home() {
                         <div className={`${productKey.toLowerCase()} pc-info`}>
                             <h3 className="pc-title">{product.name.toUpperCase()}</h3>
                             <div className='pc-icon-and-components'>
-                                <img src={BeastClaw} alt="Claw drawing." />
                                 <ul className='components-list'>
-                                    {Object.entries(product.features).map(([key, value], index) => (
+                                    {Object.entries(product.components).map(([key, value], index) => (
                                         <li key={index}>
                                             <h5 className='component-type'>{key}</h5>:
                                             <br />
@@ -198,6 +222,7 @@ export default function Home() {
                                         </li>
                                     ))}
                                 </ul>
+                                <img src={product.icon} alt="Claw drawing." />
                             </div>
                             <img className="img-pc" src={product.imageUrl1} alt={`${product.name} PC`} />
                             <div className="buy choose" onClick={() => selectProduct(product.name)}>BUY</div>
