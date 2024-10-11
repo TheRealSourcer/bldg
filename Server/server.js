16a47bbaa580563ab93c0d2006b6ff143644adf5
TheRealSourcer <bertolotti2008@gmail.com>
2024-10-11 19:45:04 -0400

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -237,7 +237,7 @@ async function getFedExAccessTokenRest() {
 
 
 
-async function validateAddressFedEx(address) {
+async function validateAddressFedEx(shippingAddress) {
     const accessToken = await getFedExAccessTokenRest(); // Ensure this function works and returns the token.
 
     const data = {
