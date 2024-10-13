1762758743f5345f5c9079c329e76d2cf0195831
TheRealSourcer <bertolotti2008@gmail.com>
2024-10-13 15:34:58 -0400

    Update shipping informatin.jsx

--- Client/src/pages/shipping information/shipping informatin.jsx
+++ Client/src/pages/shipping information/shipping informatin.jsx
@@ -2,6 +2,14 @@ export default function Cancels() {
     return (
         <main>
             <p>Shipping</p>
+            <form action="">
+                <input type="email" name="" id="" placeholder="Email"/>
+                <input type="text" placeholder="Address line 1"/>
+                <input type="text" placeholder="Address line 2"/>
+                <input type="text" placeholder="ZIP Code"/>
+                <input type="text" placeholder="state"/>
+                <input type="submit" value="CHECKOUT" />
+            </form>
         </main>
     )
 }
