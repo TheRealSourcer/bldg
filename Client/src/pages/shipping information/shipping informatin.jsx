82d6822c122d4f6ee2a1d8f7625cf6de883567dc
TheRealSourcer <bertolotti2008@gmail.com>
2024-10-14 12:41:46 -0400

    Update shipping informatin.jsx

--- Client/src/pages/shipping information/shipping informatin.jsx
+++ Client/src/pages/shipping information/shipping informatin.jsx
@@ -1,4 +1,36 @@
-export default function Cancels() {
+import { useEffect } from "react"
+
+export default function ShippingInfo() {
+
+    const handleCheckout = async () => {
+        try {
+            const response = await fetch('https://server-pc.onrender.com/create-checkout-session', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    items: cartItems.map(item => ({
+                        id: item.id,
+                        quantity: item.quantity
+                    })),
+                    userUUID: '96e68dab-867a-4c3e-9b81-b16fc84e5141' // Replace with actual UUID
+                }),
+            });
+    
+            const data = await response.json();
+            if (response.ok) {
+                // Redirect to Stripe Checkout
+                const stripe = window.Stripe('pk_test_51PbjzsAvZVlzPgF8Wtv9GAhGKbDJYS26DUOXtqEJ8MeM7fU5jQYuIS4G2BevkamozcYBOOYjbWCFmNqSDJGoFcGp00LaSzm6UA'); // Replace with your Stripe public key
+                stripe.redirectToCheckout({ sessionId: data.id });
+            } else {
+                console.error('Error during checkout:', data.error);
+            }
+        } catch (error) {
+            console.error('Error during checkout:', error);
+        }
+    };
+
     return (
         <main className="main-shipping-info">
             <h2 className="shipping-title">Shipping address</h2>
@@ -12,7 +44,7 @@ export default function Cancels() {
                     <input type="text" placeholder="ZIP" className="shipping-input fifty-input"/>
                 </div>
                 <input type="text" placeholder="State" className="shipping-input"/>
-                <input type="submit" value="CHECKOUT" className="shipping-submit"/>
+                <input type="submit" value="CHECKOUT" className="shipping-submit" onClick={handleCheckout}/>
             </form>
         </main>
     )
