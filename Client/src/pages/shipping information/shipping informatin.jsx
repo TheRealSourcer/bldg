dd70a8a5f47be730d5f4710f32aceabb133684d9
TheRealSourcer <bertolotti2008@gmail.com>
2024-10-14 10:32:11 -0400

    Update shipping informatin.jsx

--- Client/src/pages/shipping information/shipping informatin.jsx
+++ Client/src/pages/shipping information/shipping informatin.jsx
@@ -4,10 +4,14 @@ export default function Cancels() {
             <h2 className="shipping-title">Shipping</h2>
             <form action="" className="shipping-form">
                 <input type="email" name="" id="" placeholder="Email" className="shipping-input"/>
+                <input type="text" name="" id="" placeholder="Full name" className="shipping-input"/>
                 <input type="text" placeholder="Address line 1" className="shipping-input"/>
                 <input type="text" placeholder="Address line 2" className="shipping-input"/>
-                <input type="text" placeholder="ZIP Code" className="shipping-input"/>
-                <input type="text" placeholder="state" className="shipping-input"/>
+                <div>
+                    <input type="text" placeholder="City" className="shipping-input"/>
+                    <input type="text" placeholder="ZIP" className="shipping-input"/>
+                </div>
+                <input type="text" placeholder="State" className="shipping-input"/>
                 <input type="submit" value="CHECKOUT" className="shipping-submit"/>
             </form>
         </main>
