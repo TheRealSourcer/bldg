0942a6f1009007e433fc1c1ae3c36e135e985f7f
TheRealSourcer <bertolotti2008@gmail.com>
2024-10-29 19:05:10 -0400

    Changed to modules.js format instead of common.js in postcss.config.js file

--- Client/postcss.config.js
+++ Client/postcss.config.js
@@ -1,20 +1,27 @@
-module.exports = {
-    plugins: [
-      require('postcss-import'),
-      require('postcss-nested'),
-      require('postcss-preset-env')({
-        stage: 1,
-        features: {
-          'nesting-rules': true
-        }
-      }),
-      require('autoprefixer'),
-      require('cssnano')({
-        preset: ['default', {
-          discardComments: {
-            removeAll: true,
-          },
-        }]
-      })
-    ]
+// postcss.config.js
+import postcssImport from 'postcss-import';
+import postcssNested from 'postcss-nested';
+import postcssPresetEnv from 'postcss-preset-env';
+import autoprefixer from 'autoprefixer';
+import cssnano from 'cssnano';
+
+export default {
+  plugins: [
+    postcssImport,
+    postcssNested,
+    postcssPresetEnv({
+      stage: 1,
+      features: {
+        'nesting-rules': true
+      }
+    }),
+    autoprefixer,
+    process.env.NODE_ENV === 'production' && cssnano({
+      preset: ['default', {
+        discardComments: {
+          removeAll: true,
+        },
+      }]
+    })
+  ].filter(Boolean)
 }
