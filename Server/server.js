73e3cca51f8e44ba377475e7c2d1e8a5cce91062
TheRealSourcer <bertolotti2008@gmail.com>
2024-11-10 14:09:34 -0500

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -17,8 +17,8 @@ const stripe = Stripe(process.env.STRIPE_PRIVATE_KEY);
 const transporter = nodemailer.createTransport({
     service: 'gmail', // or another service
     auth: {
-        user: process.env.EMAIL_USER, // your email address
-        pass: process.env.EMAIL_PASS  // your email password
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
     }
 });
 
